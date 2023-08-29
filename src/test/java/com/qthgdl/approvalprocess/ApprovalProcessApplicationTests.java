8658f9edccf9d73a87a3ed11ab8f0bed9b8647d8
qiuy <qiuy@vlandc>
2023-08-29 14:41:58 +0800

    初始化包含子任务的流程审批项目;

---
+++ src/test/java/com/qthgdl/approvalprocess/ApprovalProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qthgdl.approvalprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
